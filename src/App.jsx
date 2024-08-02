8f23c0bec49d39f20d30258acc2503bb57bb39fc
Yashbabani0 <yashbabani09@gmail.com>
2024-08-02 09:34:44 +0530

    update for error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Nav from "./components/Nav";
-import MainHeader from "./components/MainHeader";
-import Footer from "./components/Footer";
-import MainBgImg from "./components/MainBgImg";
-import Part3 from "./components/Part3";
-import Part1 from "./components/Part1";
-import Part2 from "./components/Part2";
+import Nav from "@/components/Nav";
+import MainHeader from "@/components/MainHeader";
+import Footer from "@/components/Footer";
+import MainBgImg from "@/components/MainBgImg";
+import Part3 from "@/components/Part3";
+import Part1 from "@/components/Part1";
+import Part2 from "@/components/Part2";
 
 export default function App() {
   return (
