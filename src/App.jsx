846e0777ef2745d8b9ab2adfce1a5c0323e6bf3a
Yashbabani0 <yashbabani09@gmail.com>
2024-08-02 10:03:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "@/components/Nav";
 import MainHeader from "@/components/MainHeader";
 import Footer from "@/components/Footer";
-import MainBgImg from "@/components/MainBgImg.jsx";
+import MainBgImg from "@/components/MainBgImg";
 import Part3 from "@/components/Part3";
 import Part1 from "@/components/Part1";
 import Part2 from "@/components/Part2";
